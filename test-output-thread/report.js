59c74d2bbfa0a68e14ea4e3d070157cacd26326e
preyas <preyas.rana@zymr.com>
2022-06-16 10:44:17 +0530

    test

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"e344cdb3-5d52-4c49-9091-08f94b66e75a","feature":"Feature to Snapdeal HomePage functionality","scenario":"Verification for Snapdeal ChangePassword funcationality","start":1655354876664,"group":1,"content":"","tags":"@feature,@smoke,","end":1655354911468,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
